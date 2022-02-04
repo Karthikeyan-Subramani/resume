e29a1fcdcae7e089383db4583133926589fc4aae
Karthikeyan-Subramani <98938972+Karthikeyan-Subramani@users.noreply.github.com>
2022-02-04 14:36:58 +0530

    Update newproject.js

--- scripts/newproject.js
+++ scripts/newproject.js
@@ -3,17 +3,3 @@ $(window).scroll(function() {
   $(".brand1").toggleClass('brand1-size', $(this).scrollTop() > 20);
   $(".brand2").toggleClass('brand2-size', $(this).scrollTop() > 20);
 });
-
-$(document).ready(function() {
-  $(this).scrollTop(0);
-  $(".fa-angle-double-down").click(function() {
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    if (w < 800) {
-      window.scrollBy(0, 580);
-    } else {
-      window.scrollBy(0, 750);
-    }
-    console.log("w:" + w + "h:" + h);
-  });
-});
